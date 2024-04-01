369a4d857643b1e5dcf467c63ac1170131a1f4c7
LEE WOOJIN <24864@naver.com>
2024-04-01 18:16:26 +0900

    pj

--- 1차프로젝트/03.구현소스/js/product.js
+++ 1차프로젝트/03.구현소스/js/product.js
@@ -31,15 +31,137 @@ const prdNew = {
   },
 };
 
-const prdWrap = document.querySelector('.prdWrap')
+const prdSw = {
+  sw1: {
+    img: "product_sand1.png",
+    imghv: "product_sand_hover1.png",
+    name: "프리미엄 햄치즈 샌드위치",
+    desc: "탕종식빵과 터키브레스트 햄,통모짜렐라치즈의 만남",
+  },
+  sw2: {
+    img: "product_sand2.png",
+    imghv: "product_sand_hover2.png",
+    name: "프레시 파 베이컨 크림치즈",
+    desc: "부드러운 크림치즈에 쪽파와 베이컨이가득!",
+  },
+  sw3: {
+    img: "product_sand3.png",
+    imghv: "product_sand_hover3.png",
+    name: "매콤에그마요 총좌빙",
+    desc: "매콤한 소스와 고소한 에그마요가 듬뿍",
+  },
+  sw4: {
+    img: "product_sand4.png",
+    imghv: "product_sand_hover4.png",
+    name: "햄치즈 샌드위치",
+    desc: "폭신한 빵 속 햄과 치즈의 균형잡힌 맛",
+  },
+  sw5: {
+    img: "product_sand5.png",
+    imghv: "product_sand_hover5.png",
+    name: "에그 포테이토 샌드위치",
+    desc: "감자와 에그 샐러드의 든든한 조합",
+  },
+  sw6: {
+    img: "product_sand6.png",
+    imghv: "product_sand_hover6.png",
+    name: "갓군샌 갈릭버터",
+    desc: "고소한 버터와 국내산 마늘의 풍미 가득한 만남",
+  },
+  sw7: {
+    img: "product_sand7.png",
+    imghv: "product_sand_hover7.png",
+    name: "홍루이젠 잠봉뵈르",
+    desc: "풍미 가득 버터와 국내산 잠봉이 만나 부드러운 소프트 바게트 속으로",
+  },
+  sw8: {
+    img: "product_sand8.png",
+    imghv: "product_sand_hover8.png",
+    name: "요거베리 샌드위치",
+    desc: "상큼한 요거트 크림에 블루베리잼으로 달콤하게!",
+  },
+};
+
+const prdDst = {
+  dst1: {
+    img: "product_dessert1.png",
+    imghv: "product_dessert_hover1.png",
+    name: "우유크림 아이슈",
+    desc: "바삭한 쿠키 크럼블에 진한 우유크림이 가득찬 쿠키슈",
+  },
+  dst2: {
+    img: "product_dessert2.png",
+    imghv: "product_dessert_hover2.png",
+    name: "우유퐁당 케익샌드 초코",
+    desc: "사르르 눈처럼 부드럽고 달콤함 우유크림 케익샌드 초코",
+  },
+  dst3: {
+    img: "product_dessert3.png",
+    imghv: "product_dessert_hover3.png",
+    name: "우유퐁당 케익샌드 플레인",
+    desc: "사르르 눈처럼 부드럽고 달콤함 우유크림 케익샌드 플레인",
+  },
+  dst4: {
+    img: "product_dessert4.png",
+    imghv: "product_dessert_hover4.png",
+    name: "리얼펑리수",
+    desc: "대만 현지 레시피를 완벽하게 구현한 프리미엄 파인애플 펑리수",
+  },
+  dst5: {
+    img: "product_dessert5.png",
+    imghv: "product_dessert_hover5.png",
+    name: "에그롤",
+    desc: "고소한 계란과 깊고 부드러운 버터향 가득",
+  },
+};
+
+const prdDrk = {
+  drk1: {
+    img: "product_drink1.png",
+    imghv: "product_drink_hover1.png",
+    name: "홍밀크",
+    desc: "우유 속 비정제 흑설탕의 건강한 달콤함",
+  },
+  drk2: {
+    img: "product_drink2.png",
+    imghv: "product_drink_hover2.png",
+    name: "초콜릿 밀크",
+    desc: "달콤한 초콜릿의 진한 맛과 우유의 조화",
+  },
+};
+const prdWrap = document.querySelector('.prdWrap');
 console.log(prdWrap);
 
-evtMakeList();
+let hcode = "";
+/* 
+let prdBtn = document.querySelectorAll('.prdBtn li');
 
-function evtMakeList() {
-  let hcode = "";
-  for (let x in prdNew) {
-    let data = prdNew[x];
+let newBtn = prdBtn[0];
+let sdBtn = prdBtn[1];
+let dstBtn = prdBtn[2];
+let drkBtn = prdBtn[3];
+
+console.log("0",prdBtn[0]);
+console.log("1",prdBtn[1]);
+console.log("2",prdBtn[2]);
+console.log("3",prdBtn[3]); */
+
+let newBtn = document.querySelector(".newBtn");
+let sdBtn = document.querySelector(".sdBtn");
+let dstBtn = document.querySelector(".dstBtn");
+let drkBtn = document.querySelector(".drkBtn");
+console.log(sdBtn);
+
+sdBtn.addEventListener('click', prdMakeList(prdSw));
+
+/* prdMakeList(prdNew);
+prdMakeList(prdSw); */
+
+
+function prdMakeList(menu) {
+  console.log('ddddd');
+  for (let x in menu) {
+    let data = menu[x];
 
     hcode += `   
     <li>
@@ -63,5 +185,6 @@ function evtMakeList() {
     `;
   } // for문
   prdWrap.innerHTML = hcode;
-} // makelist
+} // prdNewList
+
 
