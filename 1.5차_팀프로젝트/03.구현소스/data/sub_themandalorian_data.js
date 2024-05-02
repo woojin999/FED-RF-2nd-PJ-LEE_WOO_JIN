8ad76562056f82e066a279635c0d7a6913fbd385
LEE WOOJIN <24864@naver.com>
2024-05-02 17:46:01 +0900

    pj

---
+++ 1.5차_팀프로젝트/03.구현소스/data/sub_themandalorian_data.js
@@ -0,0 +1,98 @@
+const videoData = [
+  {
+    image: "video1.jpeg",
+    video: "XRL7BllpSxE",
+    desc: "The Mandalorian & Grogu, Ahsoka Season 2  This Week! in Star Wars Dispatch",
+  },
+  {
+    image: "video2.jpg",
+    video: "Grr50nHfLEU",
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "video3.jpg",
+    video: "fx1xFHroPRw",
+    desc: "“Peli’s Back” Official Clip | The Mandalorian",
+  },
+  {
+    image: "video3.jpg",
+    video: "fx1xFHroPRw",
+    desc: "The Mandalorian & Grogu, Ahsoka Season 2  This Week! in Star Wars Dispatch",
+  },
+  {
+    image: "video3.jpg",
+    video: "fx1xFHroPRw",
+    desc: "The Mandalorian & Grogu, Ahsoka Season 2  This Week! in Star Wars Dispatch",
+  },
+  {
+    image: "video3.jpg",
+    video: "fx1xFHroPRw",
+    desc: "The Mandalorian & Grogu, Ahsoka Season 2  This Week! in Star Wars Dispatch",
+  },
+];
+
+const characterData = [
+  {
+    image: "character1.jpeg",
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "character2.jpeg",
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "character3.jpeg",
+    desc: "Gloval Murals | The Mandalorian",
+  },
+];
+
+const episodeData = [
+  {
+    image: "epsode1.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "The Mandalorian and his allies attempt a daring rescue.",
+  },
+  {
+    image: "epsode2.jpeg",
+    title: '"The Believer" Episode Guide | The Mandalorian',
+    desc: "To move against the Empire, the Mandalorian needs the help of an old enemy.",
+  },
+  {
+    image: "epsode3.jpeg",
+    title: '"The Tragedy" Episode Guide | The Mandalorian',
+    desc: "The Mandalorian and the Child continue their journey through a dangerous galaxy.",
+  },
+  {
+    image: "epsode4.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "epsode5.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "epsode6.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "epsode7.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "epsode8.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+  {
+    image: "epsode9.jpeg",
+    title: '"The Rescue" Episode Guide | The Mandalorian',
+    desc: "Gloval Murals | The Mandalorian",
+  },
+];
+
+export {videoData,episodeData};
+
--- 1.5차_팀프로젝트/03.구현소스/js/sub_themandalorian.js
+++ 1.5차_팀프로젝트/03.구현소스/js/sub_themandalorian.js
@@ -1,29 +1,187 @@
 import mFn from "./my_function.js";
 
+import * as mandalData from "../data/sub_themandalorian_data.js";
+
+
+const epData = mandalData.episodeData;
+console.log(epData);
+
+let eplicode = `<ul class="fx-box">`;
+
+for(let i = 0; i < 9; i++){
+  eplicode += `
+  <li>
+    <div class="ep-img-box">
+      <img src="./images/${epData[i].image}" alt="" />
+      <div class="ep-bg-box"></div>
+      <div class="ep-txt-box">
+        <h3>
+          <span class="ep-title">
+            "The Tragedy" Episode Guide | The Mandalorian
+          </span>
+        </h3>
+        <p class="ep-desc">
+          The Mandalorian and the Child continue their journey
+          through a dangerous galaxy.
+        </p>
+      </div>
+    </div>
+  </li>
+  `;
+}
+
+///////////////////////////////////////////////////////////////
+
+const epbtn = mFn.qsa(".epbtn");
+const vdbtn = mFn.qsa(".vdbtn");
+const epSlide = mFn.qsa(".ep-img-wrap ul li");
+const videoWrap = mFn.qs(".video-wrap");
+const videoli = mFn.qs(".video-list");
+const videoBox = mFn.qs(".video-cont-box");
+const videoTab = mFn.qs(".video-tab");
+
+const vdData = mandalData.videoData;
+
+
+
+let vlicode = `<ul class="fx-box">`;
+for (let i = 0; i <= 5; i++) {
+  vlicode += `
+    <li>
+      <div class="video-box">
+        <div class="video-img">
+          <img src="./images/${vdData[i].image}" alt="" />
+        </div>
+        <div class="video-txt-box">
+          <h3>
+            <span>
+              ${vdData[i].desc}
+            </span>
+          </h3>
+        </div>
+      </div>
+    </li>
+  `;
+}
+vlicode += `</ul>`;
+
+videoli.innerHTML = vlicode;
+
+const vdSlide = mFn.qsaEl(videoWrap, ".video-img-wrap ul li");
+const video = mFn.qsaEl(videoli, ".video-box");
+
+
+let vdcode = ``;
+video.forEach((ele, i) => {
+  ele.onclick = () => {
+    vdcode += `
+    <div class="video-tab-btn fa-solid fa-xmark fa-3x"></div>
+    <iframe src="https://www.youtube.com/embed/${vdData[i].video}"></iframe>
+  `;
+    videoTab.innerHTML = vdcode;
+    videoBox.classList.add("on");
+
+    mFn.qsEl(videoTab, "div").onclick = () => {
+      console.log("ddd");
+      videoBox.classList.remove("on");
+      vdcode = ``;
+    };
+  };
+});
+
+
+let epSnum = 0;
+let vdSnum = 0;
+let prot = false;
+
+for (let x of epbtn) {
+  x.onclick = GoEpSlide;
+}
+for (let x of vdbtn) {
+  x.onclick = GoVdSlide;
+}
+
+function GoEpSlide() {
+  // 광클 금지
+  if (prot) return;
+  prot = true;
+  setTimeout(() => {
+    prot = false;
+  }, 500);
+
+  let isEpRbtn = this.classList.contains("epbtn2");
+  let cntNum;
+  let epSlCnt = epSlide.length;
+
+  isEpRbtn ? epSnum++ : epSnum--;
+
+  epSlide.forEach((v) => {
+    v.style.left = -33.3333 * epSnum + "%";
+  });
+
+  cntNum = 3;
+
+  if (epSnum < 0) {
+    epSnum = 0;
+  } else if (epSnum > epSlCnt - cntNum) {
+    epSnum = epSlCnt - cntNum;
+  }
+
+  if (epSnum == 0 || epSnum == epSlCnt - cntNum) {
+    this.style.display = "none";
+  } else {
+    for (let x of epbtn) {
+      x.style.display = "block";
+    }
+  }
+}
+
+function GoVdSlide() {
+  // 광클 금지
+  if (prot) return;
+  prot = true;
+  setTimeout(() => {
+    prot = false;
+  }, 500);
+
+  let isVdRbtn = this.classList.contains("vdbtn2");
+
+  let cntNum;
+  let vdSlCnt = 5;
+  console.log(vdSlCnt);
+
+  isVdRbtn ? vdSnum++ : vdSnum--;
+
+  vdSlide.forEach((v) => {
+    v.style.left = -33.3333 * vdSnum + "%";
+  });
+
+  cntNum = 3;
+
+  if (vdSnum < 0) {
+    vdSnum = 0;
+  } else if (vdSnum > vdSlCnt - cntNum) {
+    vdSnum = vdSlCnt - cntNum;
+  }
+
+  if (vdSnum == 0 || vdSnum == vdSlCnt - cntNum) {
+    this.style.display = "none";
+  } else {
+    for (let x of vdbtn) {
+      x.style.display = "block";
+    }
+  }
+}
+
+///////////////////////////////////
+
 const crtImgBox = mFn.qs(".character-img-box");
 
-let hcode = `<div class="img-list">
+let crtcode = `<div class="img-list">
               <ul class="list01">`;
 
 for (let i = 1; i <= 10; i++) {
-    hcode += `
-        <li>
-            <div class="list-box">
-                <img src="./images/character${i}.jpeg" alt="" />
-                <h3>
-                    <span> Kelleran Beq</span>
-                </h3>
-            </div>
-        </li>
-    `;
-}          
-hcode += `
-</ul>
-</div>
-<div class="img-list">
-<ul class="list02">`;
-for (let i = 11; i <= 25; i++) {
-  hcode += `
+  crtcode += `
         <li>
             <div class="list-box">
                 <img src="./images/character${i}.jpeg" alt="" />
@@ -34,7 +192,24 @@ for (let i = 11; i <= 25; i++) {
         </li>
     `;
 }
-crtImgBox.innerHTML = hcode;
+crtcode += `
+</ul>
+</div>
+<div class="img-list">
+<ul class="list02">`;
+for (let i = 11; i <= 25; i++) {
+  crtcode += `
+        <li>
+            <div class="list-box">
+                <img src="./images/character${i}.jpeg" alt="" />
+                <h3>
+                    <span> Kelleran Beq</span>
+                </h3>
+            </div>
+        </li>
+    `;
+}
+crtImgBox.innerHTML = crtcode;
 
 let target = mFn.qsEl(crtImgBox, ".list01");
 let target2 = mFn.qsEl(crtImgBox, ".list02");
@@ -91,61 +266,3 @@ mFn.addEvt(crtImgBox, "mouseleave", () => {
 
 setTimeout(moveGallery);
 setTimeout(moveGallery2);
-
-
-///////////////////////////////////////////////////////////////
-
-const abtn = mFn.qsa(".abtn");
-const epbtn = mFn.qsa(".epbtn");
-const vdbtn = mFn.qsa(".vdbtn");
-const epSlide = mFn.qsa(".ep-img-wrap ul li");
-const vdSlide = mFn.qsa(".video-img-wrap ul li");
-let epSnum = 0;
-let vdSnum = 0;
-
-
-for(let x of abtn){
-  x.onclick = GoSlide;
-}
-
-function GoSlide() {
-  let isEpLbtn = this.classList.contains("epbtn1");
-  let isEpRbtn = this.classList.contains("epbtn2");
-  let isVdRbtn = this.classList.contains("vdbtn2");
-  let cntNum;
-  let epSlCnt = epSlide.length;
-  // console.log(epSlCnt);
-
-  isEpRbtn ? epSnum++ : isEpLbtn ? epSnum-- : isVdRbtn ? vdSnum ++ : vdSnum--;
-
-  epSlide.forEach(v=>{
-    // console.log(v,epSnum);
-    v.style.left = -33.3333 * epSnum +"%"
-  })
-
-  vdSlide.forEach(v=>{
-    // console.log(v);
-    v.style.left = -33.3333 * vdSnum + "%"
-  })
-
-  cntNum = 3;
-
-  if(epSnum < 0){
-    epSnum = 0;
-  } else if(epSnum > epSlCnt - cntNum) {
-    epSnum = epSlCnt - cntNum;
-  }
-
-  if (epSnum == 0 || epSnum == epSlCnt - cntNum) {
-    console.log(epbtn[0] == this);
-    console.log(this);
-    this.style.display = "none";
-  } else{
-    for(let x of epbtn) {
-      x.style.display = "block";
-    }
-  }
-}
-
-
-
